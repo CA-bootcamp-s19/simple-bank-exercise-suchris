e2b32fd3536d4f6454a7db09cff23edd8997964a
Christine Su <suchris@yahoo.com>
2019-05-31 22:31:22 -0400

    Complete debug

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -13,28 +13,37 @@ contract SimpleBank {
     //
     
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
+    mapping (address => uint) private balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address owner;
+    address public owner;
     
     //
     // Events - publicize actions to external listeners
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled(address account);
+    event LogEnrolled(address accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade(address account, uint amount);
+    event LogDepositMade(address accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-    event LogWithdrawal(address account, uint amount);
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
+    modifier isNotOwner() {
+        require(msg.sender != owner,"Can't be owner");
+        _;
+    }
+
+    modifier isEnrolled() {
+        require(enrolled[msg.sender], "Must be enrolled");
+        _;
+    }
     //
     // Functions
     //
@@ -57,12 +66,10 @@ contract SimpleBank {
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
-    function enroll() public returns (bool isEnrolled) {
-        if (owner != msg.sender) {
-            enrolled[msg.sender] = true;
-        }
-        isEnrolled = enrolled[msg.sender];
+    function enroll() public isNotOwner() returns (bool) {
+        enrolled[msg.sender] = true;
         emit LogEnrolled(msg.sender);
+        return true;
     }
 
     /// @notice Deposit ether into bank
@@ -71,14 +78,12 @@ contract SimpleBank {
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event
     // Users should be enrolled before they can make deposits
-    function deposit(uint depositAmount) public returns (uint currentBalance) {
+    function deposit() public payable isEnrolled() returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
-        if (enrolled[msg.sender] == true) {
-            balances[msg.sender] += depositAmount;
-        }
-        currentBalance = balances[msg.sender];
-        emit LogDepositMade(msg.sender, depositAmount);
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+        return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
@@ -86,16 +91,18 @@ contract SimpleBank {
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
     // Emit the appropriate event
-    function withdraw(uint withdrawAmount) public returns (uint currentBalance) {
+    function withdraw(uint withdrawAmount) public isEnrolled() returns (uint) {
         /* If the sender's balance is at least the amount they want to withdraw,
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw.
            return the user's balance.*/
-        if (enrolled[msg.sender] && balances[msg.sender] >= withdrawAmount) {
-            balances[msg.sender] -= withdrawAmount;
+        if (balances[msg.sender] < withdrawAmount) {
+            revert("Not enough balance to withdraw");
         }
-        currentBalance = balances[msg.sender];
-        emit LogWithdrawal(msg.sender, withdrawAmount);
+        balances[msg.sender] -= withdrawAmount;
+        msg.sender.transfer(withdrawAmount);
+        emit LogWithdrawal(msg.sender, withdrawAmount,balances[msg.sender]);
+        return balances[msg.sender];
     }
 
     // Fallback function - Called if other functions don't match call or
